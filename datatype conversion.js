358b761a1d6d56bf394c32bedf39650f517be3d9
vanshikakatlana <vanshikakatlana@gmail.com>
2024-07-23 22:31:30 +0530

    DataType file commit

---
+++ datatype conversion.js
@@ -0,0 +1,12 @@
+let a=10
+//console.log(typeof a)
+let valueInString=String(a)
+//console.log(typeof valueInString)
+
+let isLoggedIn=1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn)
+
+
+console.log("2"==2)
+console.log("2"===2)
